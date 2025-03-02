4ea88d8b66eeea84c565863caccaa33c758a0a6e
Bishwas Baral <bishwasbaral50@gmail.com>
2025-03-02 10:38:49 +0545

    solution to exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,7 +12,23 @@ const totalLikes = (blogs) => {
   : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0) {
+    return null
+  }
+  const mostLikes = Math.max(...blogs.map(blog => blog.likes))
+  const favorite = blogs.filter(blog => blog.likes === mostLikes)[0]
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }

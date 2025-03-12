b1e2cc68e98ec76c180500a46bf2b15cef9f2496
Bishwas Baral <bishwasbaral50@gmail.com>
2025-03-12 17:19:43 +0545

    solution to exercise 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,16 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const mongoose = require('mongoose')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -11,11 +20,17 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const { title, author, url, likes } = request.body
 
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+  if(!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
   if (!title || !url) {
     return response.status(400).end()
   }
 
-  const user = await User.findOne()
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: title,

15e390664ac9471340ba62c293c7bb0d599e395f
Bishwas Baral <bishwasbaral50@gmail.com>
2025-03-12 17:44:18 +0545

    solution to exercise 4.20

--- utils/middleware.js
+++ utils/middleware.js
@@ -4,6 +4,18 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+    console.log(request.token)
+  } else {
+    request.token = null
+  }
+
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
@@ -22,5 +34,6 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   unknownEndpoint,
+  tokenExtractor,
   errorHandler
 }
